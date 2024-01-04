a2d16d12b36ad09345070d4b8962bc1137715bf8
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2024-01-04 12:35:17 +0530

    remove Resources

--- main.tf
+++ main.tf
@@ -37,48 +37,48 @@ provider "aws" {
 # Add .gitignore file in this directory with the terraform.tfvars
 
 
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "GameScores"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "UserId"
-  range_key      = "GameTitle"
+# resource "aws_dynamodb_table" "basic-dynamodb-table" {
+#   name           = "GameScores"
+#   billing_mode   = "PROVISIONED"
+#   read_capacity  = 20
+#   write_capacity = 20
+#   hash_key       = "UserId"
+#   range_key      = "GameTitle"
 
-  attribute {
-    name = "UserId"
-    type = "S"
-  }
+#   attribute {
+#     name = "UserId"
+#     type = "S"
+#   }
 
-  attribute {
-    name = "GameTitle"
-    type = "S"
-  }
+#   attribute {
+#     name = "GameTitle"
+#     type = "S"
+#   }
 
-  attribute {
-    name = "TopScore"
-    type = "N"
-  }
+#   attribute {
+#     name = "TopScore"
+#     type = "N"
+#   }
 
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = false
-  }
+#   ttl {
+#     attribute_name = "TimeToExist"
+#     enabled        = false
+#   }
 
-  global_secondary_index {
-    name               = "GameTitleIndex"
-    hash_key           = "GameTitle"
-    range_key          = "TopScore"
-    write_capacity     = 10
-    read_capacity      = 10
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["UserId"]
-  }
+#   global_secondary_index {
+#     name               = "GameTitleIndex"
+#     hash_key           = "GameTitle"
+#     range_key          = "TopScore"
+#     write_capacity     = 10
+#     read_capacity      = 10
+#     projection_type    = "INCLUDE"
+#     non_key_attributes = ["UserId"]
+#   }
 
-  tags = {
-    Name        = "tc-vc-triggered-2"
-    Environment = "production"
-  }
+#   tags = {
+#     Name        = "tc-vc-triggered-2"
+#     Environment = "production"
+#   }
 
 /*resource "aws_instance" "tc_instance" {
   ami           = "ami-0c7c4e3c6b4941f0f"
@@ -88,4 +88,4 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     Name = "TC-triggered-instance"
   } */
 
-}
+#}
